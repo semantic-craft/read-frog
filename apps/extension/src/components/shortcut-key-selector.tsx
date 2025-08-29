b2cca8a72eefb7c2662d32b8ea115f690dd8eadc
taiiiyang <781547101@qq.com>
2025-08-29 14:43:28 +0800

    fix: fix conflicts

--- apps/extension/src/components/shortcut-key-selector.tsx
+++ apps/extension/src/components/shortcut-key-selector.tsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { i18n } from '#imports'
 import { Input } from '@repo/ui/components/input'
 import { cn } from '@repo/ui/lib/utils'
@@ -79,10 +80,28 @@ export function ShortcutKeySelector(
 
     return () => hotkeys.unbind()
   }, [inRecording])
+=======
+import { Input } from '@repo/ui/components/input'
+import { useRecordHotkey } from '@/hooks/use-record-hotkey'
+import { logger } from '@/utils/logger'
+
+export function ShortcutKeySelector(
+  { value, className }:
+  { value: Set<string>, className?: string },
+) {
+  const [recordDomRef, { start }] = useRecordHotkey({
+    onConfirm: (keys) => {
+      logger.log(keys, 'onConfirm')
+    },
+  })
+
+  const hotkey = Array.from(value).join('+')
+>>>>>>> a70797e (feat: implement shortcut)
 
   return (
     <Input
       ref={recordDomRef}
+<<<<<<< HEAD
       className={cn('select-none', className)}
       onFocus={startRecord}
       onBlur={endRecord}
@@ -114,3 +133,18 @@ function collectModifiers() {
   const ownModifiers = MODIFIERS.filter(modifier => hotkeys[modifier])
   return Array.from(new Set(ownModifiers))
 }
+=======
+      className={className}
+      // className={cn(
+      //   'px-2 py-1 border rounded text-sm w-60 h-12 flex items-center justify-start cursor-pointer',
+      //   'file:text-foreground placeholder:text-muted-foreground dark:bg-input/30 border-input min-w-0 border bg-transparent text-base shadow-xs transition-[color,box-shadow] outline-none ',
+      //   'focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px]',
+      //   inRecording ? 'border-blue-500' : '',
+      //   className,
+      // )}
+      value={hotkey}
+      onDoubleClick={start}
+    />
+  )
+}
+>>>>>>> a70797e (feat: implement shortcut)
