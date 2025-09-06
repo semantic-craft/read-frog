81971c69afcbb57afc2559342ce00f8c895c02eb
ananaBMaster <68643891+ananaBMaster@users.noreply.github.com>
2025-09-06 13:12:07 -0700

    fix: import from vitest

--- apps/extension/src/utils/host/translate/api/__tests__/free-api.test.ts
+++ apps/extension/src/utils/host/translate/api/__tests__/free-api.test.ts
@@ -1,3 +1,4 @@
+import { describe, expect, it } from 'vitest'
 import { DEFAULT_DEEPLX_CONFIG } from '@/utils/constants/config'
 import { deeplxTranslate, googleTranslate, microsoftTranslate } from '../../api'
 
