5a4362e0beea5b90a0b171f0cd4eb408773c0588
ananaBMaster <68643891+ananaBMaster@users.noreply.github.com>
2025-09-11 10:01:55 -0700

    fix: fix the version of icon-static-svg

--- apps/extension/src/utils/logo.ts
+++ apps/extension/src/utils/logo.ts
@@ -1,3 +1,3 @@
 export function getLobeIconsCDNUrl(iconSlug: string) {
-  return `https://registry.npmmirror.com/@lobehub/icons-static-svg/latest/files/icons/${iconSlug}.svg`
+  return `https://registry.npmmirror.com/@lobehub/icons-static-svg/1.65.0/files/icons/${iconSlug}.svg`
 }
