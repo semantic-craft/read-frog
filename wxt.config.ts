fc0f5059677c71b00455268c9a2ad886f8b0b64d
Yukiniro <yukiniro@hotmail.com>
2025-12-02 23:59:29 +0800

    feat: add OAuth2 client ID configuration to manifest

--- wxt.config.ts
+++ wxt.config.ts
@@ -24,6 +24,11 @@ export default defineConfig({
       key: 'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAw2KhiXO2vySZtPu5pNSbyKhYavh8Be7gXmCZt8aJf6tQ/L3JK0qzL+3JSc/o20td3Jw+B2Dcw+EI93NAZr24xKnTNXQiJpuIuHb8xLXD0Ra/HrTVi4TJIhPdESogoG4uL6CD/F3TxfZJ2trX4Bt9cdAw1RGGeU+xU0g+YFfEka4ZUCpFAmTEw9H3/DU+nCp8yGaJWyiVgCTcFe38GZKEPt0iMJkTw956wz/iiafLx0pNG/RaztG9cAPoQOD2+SMFaeQ+b/G4OG17TYhzb09AhNBl6zSJ3jTKHSwuedCFwCce8Q/EchJfQZv71mjAE97bzwvkDYPCLj31Z5FE8HntMwIDAQAB',
     }),
     permissions: ['storage', 'tabs', 'alarms', 'cookies', 'identity'],
+    manifest: () => ({
+      oauth2: {
+        client_id: import.meta.env.WXT_GOOGLE_CLIENT_ID,
+      },
+    }),
     host_permissions:
       mode === 'development'
         ? [
